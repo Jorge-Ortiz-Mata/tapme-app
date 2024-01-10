29be6043b232c25c18511733525b87f129a5cfbd
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2024-01-10 15:18:24 -0600

    Initial Commit

---
+++ CapFun/CapFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapFunApp.swift
+//  CapFun
+//
+//  Created by Jorge on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CapFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
